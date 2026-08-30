03c531fd5df8096dcc836522f1ef76da201b9590
Kapil Londhe <kapillondhe25@gmail.com>
2026-08-30 09:24:47 +0530

    Update database engine config

--- app/core/database.py
+++ app/core/database.py
@@ -1,16 +1,24 @@
 
 from collections.abc import AsyncGenerator
+from uuid import uuid4
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.pool import NullPool
 
 from app.core.config import settings
+
 
 
 engine = create_async_engine(
     settings.database_url,
     echo=False,
-    connect_args={"statement_cache_size": 0},
+    poolclass=NullPool,
+    connect_args={
+        "statement_cache_size": 0,
+        "prepared_statement_cache_size": 0,
+        "prepared_statement_name_func": lambda: f"__asyncpg_{uuid4()}__",
+    },
 )
 
 
